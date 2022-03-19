173d61324b73991aebb4930da3cc241dfb7b7f7c
Calisto184AC0 <arturorjach@gmail.com>
2022-03-19 23:28:24 +0100

    configurando proyecto

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,25 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'node': true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended'
-    ],
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    'plugins': [
-        'react'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
-    }
+    plugins: ['react'],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'no-unused-vars': 'warn',
+    },
 }
