e43ffeda65c4989c1cfffe500941e622606df263
Calisto184AC0 <arturorjach@gmail.com>
2022-04-26 20:42:18 +0200

    imagenes subidas

---
+++ src/assets/images/Diseno/Formato/index.js
@@ -0,0 +1,99 @@
+import select20x120 from './selects/120x20.svg'
+import select30x60 from './selects/60x30.svg'
+import select30x90 from './selects/90x60.svg'
+import select30x120 from './selects/120x30.svg'
+import select40x80 from './selects/80x40.svg'
+import select40x120 from './selects/120x40.svg'
+import select45x90 from './selects/90x45.svg'
+import select50x100 from './selects/100x50.svg'
+import select50x120 from './selects/120x50.svg'
+import select60x60 from './selects/60x60.svg'
+import select60x90 from './selects/90x60.svg'
+import select60x120 from './selects/120x60.svg'
+import select80x80 from './selects/80x80.svg'
+
+import selectMODULAR1 from './selects/a.svg'
+import selectMODULAR2 from './selects/b.svg'
+import selectMODULAR3 from './selects/c.svg'
+import selectMODULAR4 from './selects/d.svg'
+import selectMODULAR5 from './selects/e.svg'
+import selectMODULAR6 from './selects/f.svg'
+import selectMODULAR7 from './selects/g.svg'
+import selectMODULAR8 from './selects/i.svg'
+import selectMODULAR9 from './selects/j.svg'
+
+import bckgr20x120 from './backgrounds/Dolcker Intercambiador 20x120.png'
+import bckgr30x60 from './backgrounds/Dolcker Intercambiador 30x60.png'
+import bckgr30x90 from './backgrounds/Dolcker Intercambiador 30x90.png'
+import bckgr30x120 from './backgrounds/Dolcker Intercambiador 30x120.png'
+import bckgr40x80 from './backgrounds/Dolcker Intercambiador 40x80.png'
+import bckgr40x120 from './backgrounds/Dolcker Intercambiador 40x120.png'
+import bckgr45x90 from './backgrounds/Dolcker Intercambiador 45x90.png'
+import bckgr50x100 from './backgrounds/Dolcker Intercambiador 50x100.png'
+import bckgr50x120 from './backgrounds/Dolcker Intercambiador 50x120.png'
+import bckgr60x60 from './backgrounds/Dolcker Intercambiador 60x60.png'
+import bckgr60x90 from './backgrounds/Dolcker Intercambiador 60x90.png'
+import bckgr60x120 from './backgrounds/Dolcker Intercambiador 60x120.png'
+import bckgr80x80 from './backgrounds/Dolcker Intercambiador 80x80.png'
+
+import bckgrMODULAR1 from './backgrounds/Dolcker Intercambiador MODULAR 1.png'
+import bckgrMODULAR2 from './backgrounds/Dolcker Intercambiador MODULAR 2.png'
+import bckgrMODULAR3 from './backgrounds/Dolcker Intercambiador MODULAR 3.png'
+import bckgrMODULAR4 from './backgrounds/Dolcker Intercambiador MODULAR 4.png'
+import bckgrMODULAR5 from './backgrounds/Dolcker Intercambiador MODULAR 5.png'
+import bckgrMODULAR6 from './backgrounds/Dolcker Intercambiador MODULAR 6.png'
+import bckgrMODULAR7 from './backgrounds/Dolcker Intercambiador MODULAR 7.png'
+import bckgrMODULAR8 from './backgrounds/Dolcker Intercambiador MODULAR 8.png'
+import bckgrMODULAR9 from './backgrounds/Dolcker Intercambiador MODULAR 9.png'
+
+const backgrounds = [
+    bckgr20x120,
+    bckgr30x60,
+    bckgr30x90,
+    bckgr30x120,
+    bckgr40x80,
+    bckgr40x120,
+    bckgr45x90,
+    bckgr50x100,
+    bckgr50x120,
+    bckgr60x60,
+    bckgr60x90,
+    bckgr60x120,
+    bckgr80x80,
+    bckgrMODULAR1,
+    bckgrMODULAR2,
+    bckgrMODULAR3,
+    bckgrMODULAR4,
+    bckgrMODULAR5,
+    bckgrMODULAR6,
+    bckgrMODULAR7,
+    bckgrMODULAR8,
+    bckgrMODULAR9,
+]
+
+const selects = [
+    select20x120,
+    select30x60,
+    select30x90,
+    select30x120,
+    select40x80,
+    select40x120,
+    select45x90,
+    select50x100,
+    select50x120,
+    select60x60,
+    select60x90,
+    select60x120,
+    select80x80,
+    selectMODULAR1,
+    selectMODULAR2,
+    selectMODULAR3,
+    selectMODULAR4,
+    selectMODULAR5,
+    selectMODULAR6,
+    selectMODULAR7,
+    selectMODULAR8,
+    selectMODULAR9,
+]
+
+export { backgrounds, selects }
