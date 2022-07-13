5b05c081ea77a2527d25450cfb5fa787be29a7c8
Calisto184AC0 <arturorjach@gmail.com>
2022-07-13 11:00:12 +0200

    cambiar imagenes del intercambiador

--- src/configs/EN/configVolumenVariacion/fondos/09.jsx
+++ src/configs/EN/configVolumenVariacion/fondos/09.jsx
@@ -4,7 +4,7 @@ const fondosImport = import.meta.globEager(
     '../../../../assets/images/VolumenVariacion/Fondos/I/**'
 )
 const selectoresImport = import.meta.globEager(
-    '../../../../assets/images/VolumenVariacion/Selectores/I/**'
+    '../../../../assets/images/VolumenVariacion/Iconos/**'
 )
 
 const fondos = getDataFromModules(fondosImport)
@@ -27,47 +27,47 @@ const configI = {
                 quitarFondo: true,
             },
             {
-                selectorImg: selectores[0],
+                selectorImg: selectores[1],
                 titulo: 'Part 1',
                 primerPlano: fondos[0],
             },
             {
-                selectorImg: selectores[1],
+                selectorImg: selectores[2],
                 titulo: 'Part 2',
                 primerPlano: fondos[1],
             },
             {
-                selectorImg: selectores[2],
+                selectorImg: selectores[3],
                 titulo: 'Part 3',
                 primerPlano: fondos[2],
             },
             {
-                selectorImg: selectores[3],
+                selectorImg: selectores[4],
                 titulo: 'Part 4',
                 primerPlano: fondos[3],
             },
             {
-                selectorImg: selectores[4],
+                selectorImg: selectores[5],
                 titulo: 'Part 5',
                 primerPlano: fondos[4],
             },
             {
-                selectorImg: selectores[5],
+                selectorImg: selectores[6],
                 titulo: 'Part 6',
                 primerPlano: fondos[5],
             },
             {
-                selectorImg: selectores[6],
+                selectorImg: selectores[7],
                 titulo: 'Part 7',
                 primerPlano: fondos[6],
             },
             {
-                selectorImg: selectores[7],
+                selectorImg: selectores[8],
                 titulo: 'Part 8',
                 primerPlano: fondos[7],
             },
             {
-                selectorImg: selectores[8],
+                selectorImg: selectores[9],
                 titulo: 'Part 9',
                 primerPlano: fondos[8],
             },
--- src/configs/configVolumenVariacion/fondos/14.jsx
+++ src/configs/configVolumenVariacion/fondos/14.jsx
@@ -4,7 +4,7 @@ const fondosImport = import.meta.globEager(
     '../../../assets/images/VolumenVariacion/Fondos/N/**'
 )
 const selectoresImport = import.meta.globEager(
-    '../../../assets/images/VolumenVariacion/Selectores/N/**'
+    '../../../assets/images/VolumenVariacion/Iconos/**'
 )
 
 const fondos = getDataFromModules(fondosImport)
@@ -27,47 +27,47 @@ const configN = {
                 quitarFondo: true,
             },
             {
-                selectorImg: selectores[0],
+                selectorImg: selectores[1],
                 titulo: 'Pieza 1',
                 primerPlano: fondos[0],
             },
             {
-                selectorImg: selectores[1],
+                selectorImg: selectores[2],
                 titulo: 'Pieza 2',
                 primerPlano: fondos[1],
             },
             {
-                selectorImg: selectores[2],
+                selectorImg: selectores[3],
                 titulo: 'Pieza 3',
                 primerPlano: fondos[2],
             },
             {
-                selectorImg: selectores[3],
+                selectorImg: selectores[4],
                 titulo: 'Pieza 4',
                 primerPlano: fondos[3],
             },
             {
-                selectorImg: selectores[4],
+                selectorImg: selectores[5],
                 titulo: 'Pieza 5',
                 primerPlano: fondos[4],
             },
             {
-                selectorImg: selectores[5],
+                selectorImg: selectores[6],
                 titulo: 'Pieza 6',
                 primerPlano: fondos[5],
             },
             {
-                selectorImg: selectores[6],
+                selectorImg: selectores[7],
                 titulo: 'Pieza 7',
                 primerPlano: fondos[6],
             },
             {
-                selectorImg: selectores[7],
+                selectorImg: selectores[8],
                 titulo: 'Pieza 8',
                 primerPlano: fondos[7],
             },
             {
-                selectorImg: selectores[8],
+                selectorImg: selectores[9],
                 titulo: 'Pieza 9',
                 primerPlano: fondos[8],
             },
